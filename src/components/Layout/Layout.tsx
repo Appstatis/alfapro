61d4824a80469829653f7f403812b481adf1f189
Gibsol <gibsol75@gmail.com>
2023-05-11 16:12:34 +0300

    Responsive Layout
    Add responsive padding to the Layout component.

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { Navbar } from "./Navbar";
 
 type Props = {
   children: React.ReactNode;
 };
 
 export const Layout = ({ children }: Props) => {
-  return <div>{children}</div>;
+  return (
+    <>
+      <Navbar title="AlfaPro" />
+      <div className="px-4 py-1 sm:py-2 md:py-4 lg:py-6 xl:py-8 sm:px-8 md:px-16 lg:px-24 xl:px-32">
+        {children}
+      </div>
+    </>
+  );
 };
