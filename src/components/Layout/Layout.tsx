06a9af22703d3139260a597120e49e1895b227ae
Gibsol <gibsol75@gmail.com>
2023-05-11 16:28:47 +0300

    Use exceptions in the Layout component for 404
    - Not showing Navbar on 404 page
    - Not using padding on 404 page

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,4 +1,6 @@
+import { useRouter } from "next/router";
 import React from "react";
+
 import { Navbar } from "./Navbar";
 
 type Props = {
@@ -6,10 +8,15 @@ type Props = {
 };
 
 export const Layout = ({ children }: Props) => {
+  const router = useRouter();
+
   return (
     <>
-      <Navbar title="AlfaPro" />
-      <div className="px-4 py-1 sm:py-2 md:py-4 lg:py-6 xl:py-8 sm:px-8 md:px-16 lg:px-24 xl:px-32">
+      {router.pathname !== "/404" && <Navbar title="AlfaPro" />}
+      <div
+        className={`${router.pathname !== "/404" &&
+          "px-4 py-1 sm:py-2 md:py-4 lg:py-6 xl:py-8 sm:px-8 md:px-16 lg:px-24 xl:px-32"}`}
+      >
         {children}
       </div>
     </>
