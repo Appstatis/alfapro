7181f25cbdb54ba4ea4f209ad5d6bc40f0eb6a46
Gibsol <gibsol75@gmail.com>
2023-05-12 16:03:19 +0300

    Add image to the index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,17 @@
+import Image from "next/image";
+
+import AlfaProImage from "@/assets/alfapro.jpg";
+
 const Home = () => {
-  return <div>Hello World</div>;
+  return (
+    <main>
+      <Image
+        src={AlfaProImage}
+        alt="AlfaPro index image"
+        className="h-auto max-w-full rounded-xl"
+      />
+    </main>
+  );
 };
 
 export default Home;
