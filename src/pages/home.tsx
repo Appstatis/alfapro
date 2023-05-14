09e50cd282541b9d7515427f0e57e0a3dc95a67f
Gibsol <gibsol75@gmail.com>
2023-05-14 08:55:39 +0300

    Add second section of the homepage. Remove <main />
    - Add section with grid layout and in it: one h2 and several paragraphs.
    - Change the main tag to React Fragment

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -28,7 +28,7 @@ const Home = () => {
   ];
 
   return (
-    <main>
+    <>
       <section className="grid gap-5 md:grid-cols-2">
         <div className="flex flex-col gap-2">
           <h2 className="text-2xl">Tere tulemast Alfa Pro Autokooli.</h2>
@@ -54,7 +54,53 @@ const Home = () => {
           className="h-auto max-w-full rounded-xl"
         />
       </section>
-    </main>
+      <section className="grid gap-3">
+        <h2 className="text-2xl">
+          AUTOKOOLI TEGEVUSE KVALITEEDI TAGAMISE ALUSED
+        </h2>
+        <p>
+          Autokool Alfa Pro tegutseb Haridus- ja Teadusministeeriumi poolt
+          väljastatud tegevuslubade alusel. Iga koolituse liigi kohta
+          koostatakse õppekava, mis vastab õigusaktide nõuetele.
+        </p>
+        <p>
+          Õppekavasid koostatakse õpiväljundipõhiselt ehk lähtudes sellest,
+          milliseid teadmisi ja oskusi peab õppur koolituse käigus omandama.
+          Seejuures võetakse arvesse autokooli juhtkonna ja töötajate kogemusi
+          ning Transpordiameti kehtivaid eksaminõudeid. Õppekavade koostamisel
+          tehakse koostööd teiste erialade spetsialistidega, kes annavad oma
+          erapooletu hinnangu.
+        </p>
+        <p>
+          Autokoolis Alfa Pro töötavad ainult professionaalsed õpetajad, kes
+          omavad kehtivat mootorsõidukijuhi õpetaja tunnistust ja kutselise
+          autojuhi kehtivat tervisetõendit. Õpetajad läbivad regulaarselt
+          erialaseid täienduskoolitusi ning osalevad erialastel seminaridel ja
+          infopäevadel.
+        </p>
+        <p>
+          Autokoolis kasutatavad õppesõidukid on oma tehniliste omaduste poolest
+          sarnased Eesti tavapäraste vastava kategooria sõidukitega. See aitab
+          autokooli lõpetanud õppuritel saada liikluses hakkama nii riiklikul
+          sõidueksamil kui ka hiljem, kui hakatakse kasutama isiklikku sõidukit.
+          Õppesõidukid läbivad nõuetekohase õppesõiduki ülevaatuse ja vastavad
+          õigusaktide nõuetele. Iga õpetaja vastutab selle eest, et tema
+          kasutuses olev sõiduk oleks nii seest kui ka väljast puhas.
+        </p>
+        <p>
+          Autokool Alfa Pro kasutab ka elektroonilist õppekeskkonda, kus õppurid
+          teevad kodutöid ja sooritavad teooriaeksameid. E-keskkonna
+          teenusepakkujaks on Liikluslab Baltic OÜ ja Teooria.ee.
+        </p>
+        <p>
+          Autokool Alfa Pro pakub oma õpilastele võimalust jätta tagasisidet
+          koolituse kohta. Seda võimalust pakutakse pärast koolituse lõpetamist
+          ja tagasisidet on võimalik jätta nii suuliselt kui ka e-posti teel.
+          Tagasisidet kogutakse ka jooksvalt, esitades õpilastele küsimusi
+          õppetöö käigus.
+        </p>
+      </section>
+    </>
   );
 };
 
