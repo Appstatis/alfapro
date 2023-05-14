6f491fc81cc69e5b915ffc82fec929b4e55bf542
Gibsol <gibsol75@gmail.com>
2023-05-14 11:42:44 +0300

    Add Margin top to Layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -15,7 +15,7 @@ export const Layout = ({ children }: { children: React.ReactNode }) => {
       <div
         className={`${router.pathname !== "/404" &&
           router.pathname !== "/" &&
-          "mb-2 md:mb-4 grid gap-5 px-4 py-1 sm:py-2 md:py-4 lg:py-6 xl:py-8 sm:px-16 md:px-16 lg:px-48 xl:px-64"}`}
+          "mt-3 mb-2 md:mb-4 grid gap-5 px-4 py-1 sm:py-2 md:py-4 lg:py-6 xl:py-8 sm:px-16 md:px-16 lg:px-48 xl:px-64"}`}
       >
         {children}
       </div>
