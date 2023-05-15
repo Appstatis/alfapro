0aff08333fbfa1f94b0792aef39877129b3822f2
Gibsol <gibsol75@gmail.com>
2023-05-15 11:11:02 +0300

    Autokool page implementation

---
+++ src/pages/autokool.tsx
@@ -0,0 +1,202 @@
+import Image from "next/image";
+
+import AlfaProImage from "@/assets/alfapro.jpg";
+import { StyledLink } from "@/components/StyledLink";
+
+const Autokool = () => {
+  return (
+    <>
+      <section className="grid gap-2">
+        <h1 className="text-3xl">Autokool</h1>
+        <p>
+          ALFA PRO OÜ-le on Haridus- ja Teadusministeeriumi poolt väljastatud
+          tegevusluba mootorsõidukijuhtide koolitamiseks B-kategoorias. Alus:
+          ministri 31.08.2012 käskkiri nr 425
+        </p>
+        <p>Koolitus toimub eesti ja vene keeles.</p>
+        <p>
+          Koolitus autokoolis toimub vastavalt poolt koostatud õppekavale
+          Autokool ja kooskõlastatud Maanteeametiga.
+        </p>
+        <div className="flex flex-col gap-1">
+          <StyledLink href="/AUTOKOOLI_KOOLITUSLEPING.odt">
+            Autokooli Koolitusleping
+          </StyledLink>
+          <StyledLink href="/B-kategooria-oppekava-ALFA_PRO_OU.pdf">
+            B-kategooria mootorsõiduki juhi ettevalmistamise õppekava
+          </StyledLink>
+        </div>
+      </section>
+      <section className="grid gap-2">
+        <h2 className="text-2xl">Koolituskursusele registreerimine</h2>
+        <p>
+          Koolituskursusele registreerimiseks peab Õpilasel olema kehtiv
+          isikutunnistus või muu isikut ja elamisluba tõendav dokument.
+        </p>
+        <p>
+          Koolituskursusele võetakse õppima B-kategooria mootorsõiduki
+          juhtimisõigust taotlev Õpilane, kes on õppetöö alustamise ajaks
+          vähemalt 15,5-aastane.
+        </p>
+        <p>
+          Koolituskursusele vastuvõtmiseks peab Õpilane omama taotletava
+          kategooria mootorsõiduki juhtimist lubava märkega kehtivat
+          tervisetõendit.
+        </p>
+        <p>
+          Kui koolituslepingu sõlmimise ajal on Õpilane alla 18-aastane isik
+          (alaealine), on Õpilane kohustatud teavitama koolituslepingu
+          sõlmimisest oma seaduslikku esindajat. Juhul, kui Koolituse eest
+          tasuja ei ole Õpilane, peab Õpilane teatama autokoolile koolituse eest
+          maksnud isiku nime ja isikukoodi.
+        </p>
+        <p>
+          Koolituskursusele registreerimiseks peab Õpilane täitma Autokooli
+          kodulehel www.alfapro.ee registreerimisvormi. Registreerimisvormi
+          kättesaamisel teavitab Autokool Õpilast tema valitud kursusele
+          registreerimisest ja edastab Lepingu allkirjastamiseks e-posti teel.
+          Peale Lepingu allkirjastamise aktiveerib Autokool Õpilasele konto
+          e-keskkonnas ja edastab lisaks Õpilasele konto kasutajanime ja
+          parooli. Nii auditoorse kui ka elektroonilise õppevormis korraldava
+          liiklusteooriakursuse korraldamise ja läbiviimise aja, asukoha ja
+          viisi kohta teavitab Autokool Õpilast hiljemalt Lepingu sõlmimise
+          kuupäeval.
+        </p>
+      </section>
+      <section className="grid gap-2">
+        <h2 className="text-2xl">Liiklusteooria</h2>
+        <p>
+          Autokool pakub võimaluse läbida liiklusteooriakursust auditoorses või
+          elektroonilises õppevormis. Liikluslabi elektroonilises keskkonnas või
+          Teooria.ee. Elektroonilises õppevormis jääb Õpilasel võimalus esitada
+          õppejõule küsimusi ja saada enda esitatud küsimustele vastuseid.
+          Autokool võib Õpilase õppel kasutada täiendavalt ka iseseisvat õpet,
+          mida ei arvestata vähimate õppemahtude hulka.
+        </p>
+        <p>
+          Pärast teooriakursuse läbimist peab Õpilane sooritama teooriaeksami.
+          Autokool edastab Õpilasele individuaalse koodi teooriaeksami
+          sooritamiseks e-keskkonna kaudu. Õpilane võib kasutada edastatud koodi
+          eksami sooritamiseks 30 päeva jooksul tasuta.
+        </p>
+        <p>
+          Õppesõidu aeg lepitakse eraldi kokku mootorsõidukijuhi õpetajaga
+          (edaspidi ka Õpetaja) , kellegavon Autokoolil sõlmitud Teenuste
+          osutamiseks leping.
+        </p>
+        <p>
+          Juhul, kui Õpilane ei saa Õpetajaga kokkulepitud sõidutunni ajal
+          osaleda, peab Õpilane sellest Õpetajat teavitada vähemalt 24 tundi
+          enne sõidutunni algust.
+        </p>
+        <p>
+          Õpilastel, kes on läbinud kohustuslikud teooria- ja sõidutunnid ning
+          kellel ei ole Autokooli ees rahalist võlgnevust, on õigus sooritada
+          kooli sõidueksamit. Sõidueksami sooritamiseks antakse Õpilasele kolm
+          (3) katset. Alates neljandast (4) katsest võetakse Õpilaselt
+          sõidueksami sooritamiseks lisatasu vastavalt kehtivale hinnakirjale.
+          Kooli sõidueksami läbikukkumise korral toimub korduseksam vähemalt ühe
+          (1) nädala pärast tehtud vigade analüüsimist.
+        </p>
+        <p>
+          Pärast koolituskursuse täielikku läbimist ja eksamite sooritamist
+          väljastab Autokool Õpilase algatusel viimasele mootorsõidukijuhi
+          koolituskursuse tunnistuse, mis annab Õpilasele õiguse taotleda
+          vastava kategooria juhtimisõiguse Transpordiameti teenindusbüroos.
+        </p>
+        <p>
+          Kui Õpilane soovib lõpetada koolituse või minna üle teise autokooli,
+          väljastatakse talle tõend teooria ja läbitud sõidutundide kohta ning
+          koolituskaardi koopia.
+        </p>
+      </section>
+      <section className="grid gap-2">
+        <h2 className="text-2xl">Õppetöö korralduslikud alused</h2>
+        <p>
+          Koolitus vastab liiklusseaduse, MKMi määruse nr 60 „Mootorsõidukijuhi
+          ettevalmistamise tingimused ja kord ning mootorsõidukijuhi
+          ettevalmistamise õppekavad“ ning MKMi määruse nr 54 „Nõuded
+          mootorsõidukijuhi koolitaja õppevahenditele, õppeväljakutele ja
+          õppesõidukitele“ nõuetele. Koolituse lõpetanu saab koolituskursuse
+          tunnistuse.
+        </p>
+        <p>
+          Juhi esmaõpe jaguneb alg- ja lõppastme õppeks. Algastme õpe lõpeb
+          riiklike eksamite sooritamisega Maanteeametis ja esmase juhiloa
+          saamisega. Lõppastme õppe pikkus on 23 kuud, mille käigus täiendab
+          esmase juhiloa omanik oma oskusi liikluses iseseisvalt ning läbib
+          lõppastme koolituse. Nii alg- kui ka lõppastmes toimub juhi
+          ettevalmistamine liiklusteooriaõppe ja õppesõitude vormis.
+        </p>
+        <p>
+          Liiklusteooriaõppe aineid läbitakse üldjuhul õppekavas toodud
+          järjestuses. Teoorialoenguid korraldatakse nõuetekohases õppeklassis.
+          Ühe teooriatunni pikkus on 45 minutit. Õppevormiks on auditoorne
+          õppevorm, iseseisev töö ja praktikum. Praktikumi vormis toimub
+          õppeaine „Libedasõidu riskivältimise praktikum“ ning võimaluse korral
+          õppeaine „Pimeda ajal sõiduki juhtimine“. Algastme õppes antakse iga
+          loengu lõpus iseseisev töö. Selle eesmärk on materjali süvendatud
+          õppimine ja loengus saadud teadmiste kinnistamine. Iseseisva töö
+          osakaal on individuaalne.
+        </p>
+        <p>
+          Auditoorse õppevormi asemel võidakse õppuri soovil kasutada
+          elektroonilist õppevormi. Sel juhul toimub auditoorses vormis kuus
+          õppetundi, kus käsitletakse MKMi määruse 60 lisas 6 sätestatud
+          temaatikat. Elektroonilise õppevormi kasutamisel on auditoorse õppetöö
+          osakaal vähemalt 21%.
+        </p>
+        <p>
+          Õppesõitude minimaalne maht on 30 sõidutundi, tegelik maht on
+          individuaalne. Õppesõitu korraldatakse nõuetekohase õppesõidukiga. Ühe
+          sõidutunni pikkus on 45 minutit. Iga õppesõidu lõpus märgib õpetaja
+          õpingukaardile õppesõidutundide arvu ja hinnangu õpilase oskuste
+          kohta. Hindamissüsteem: sooritatud/mittesooritatud (+/-). Õppesõitude
+          teemad läbitakse üldjuhul õppekavas toodud järjestuses.
+          Linnaliiklusesse lubatakse õppurit pärast õppeaine „Sõiduki
+          käsitsemine“ läbimist. Õppeaine „Sõit individuaalprogrammi järgi“
+          läbimine ei ole kohustuslik. Sõiduõppe aine „Pimeda ajal sõiduki
+          juhtimine“ toimub kas simulaatoril või tavalise sõidutunni vormis.
+          Viimasel juhul toimub sõidutund pimeda ajal ning sõidetakse enamasti
+          valgustamata asulavälistel teedel. Sõiduõppe ained „Algastme
+          libedasõidu harjutuste sooritamine“ ja „Libedasõidu riskivältimise
+          harjutuste sooritamine“ toimuvad selleks ettenähtud libedasõidu
+          õppeväljakul.
+        </p>
+        <p>
+          Sõiduõppe raames tehakse võimaluse korral koostööd juhendajaga,
+          kellega õpilane läbib sõidupraktikat. Sel juhul koostatakse koos
+          juhendajaga individuaalne sõidupraktika plaan, mille eesmärk on
+          kinnistada sõiduõppe käigus omandatud sõiduoskusi.
+        </p>
+        <p>
+          Jätkuõppe kaudu valmistatakse ette juhtimisõiguse taotlejaid, kes ei
+          ole riiklikku sõidueksamit kolme korraga sooritanud. Selle eesmärk on
+          juhi süvendatud ettevalmistamine, et tagada vajalike teadmiste ja
+          oskuste omandamine. Juhi jätkuõpe toimub üldjuhul õppesõidu vormis,
+          selle sisu määrab sõiduõpetaja, lähtudes sõidueksami mittesooritamise
+          põhjustest. Jätkuõppe minimaalne maht on kaks sõidutundi.
+        </p>
+      </section>
+      <section className="grid gap-2">
+        <p>
+          Isikuandmete töötlemine toimub Euroopa Parlamendi ja nõukogu 27.
+          aprilli määruses (EL) 2016/679 füüsiliste isikute kaitse kohta
+          isikuandmete töötlemisel ja selliste andmete vaba liikumise ning
+          direktiivi 96/95/46/EÜ kehtetuks tunnistamise kohta (isikuandmete
+          kaitse üldmäärus), isikuandmete kaitse seaduses, muudes asjakohastes
+          õigusaktides ning käesolevates isikuandmete töötlemise põhimõtetes
+          sätestatud nõuete kohaselt.
+        </p>
+        <p>
+          Autokool viitab isikuandmete töötlemise põhimõtetele Õpilasega Lepingu
+          sõlmimisel, talle teenuste, toodete ja/või e-keskkonna pakkumisel.
+          Lisaks, Autokool võimaldab Õpilasele käesolevate isikuandmete
+          töötlemise põhimõtetega tutvuda.
+        </p>
+      </section>
+    </>
+  );
+};
+
+export default Autokool;
