a600ecb9879834fe8cfc95e1db828996015fb391
Gibsol <gibsol75@gmail.com>
2023-05-16 16:59:04 +0300

    Create a nested div in Layout with flex-col and gap-3

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -17,7 +17,7 @@ export const Layout = ({ children }: { children: React.ReactNode }) => {
           router.pathname !== "/" &&
           "flex-1 mt-3 mb-2 md:mb-4 grid gap-5 px-4 py-1 sm:py-2 md:py-4 lg:py-6 xl:py-8 sm:px-16 md:px-16 lg:px-48 xl:px-64"}`}
       >
-        {children}
+        <div className="flex flex-col gap-3">{children}</div>
       </div>
       <Footer title="Alfa Pro Autokool" />
     </div>
