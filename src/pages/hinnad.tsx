6d7aeb3c4864b6869edb5adbce49263699555079
Gibsol <gibsol75@gmail.com>
2023-05-16 20:45:55 +0300

    Hinnad page implementation
    This page only includes text information.

---
+++ src/pages/hinnad.tsx
@@ -0,0 +1,121 @@
+const Hinnad = () => {
+  return (
+    <>
+      <h1 className="text-3xl">Hinnad</h1>
+      <section>
+        <p className="font-bold">
+          Maksesüsteem 300 € teooriaosa + 1000 € sõidutundi.
+        </p>
+        <div>
+          <p>Hind sisaldab:</p>
+          <ol className="list-decimal">
+            <li>30 sõidukorda (ühe sõidukorra kestvus on 45 minutit),</li>
+            <li>28 teooriatundi,</li>
+            <li>koolisisene teooria- ja praktilise sõidu eksam.</li>
+          </ol>
+        </div>
+      </section>
+      <section>
+        <p>Peale õppekulude, tuleb Teil teha veel järgmisi kulutusi:</p>
+        <ol className="list-disc">
+          <li>
+            Arstitõend Teie tervisliku seisundi kohta. Selle tõendi võite kätte
+            saada mistahes polikliinikus.
+          </li>
+          <li>
+            Peale selle, on iga algaja autojuht kohustatud läbima
+            esmaabikoolitus. Need kursused viiakse läbi meie autokoolis, nende
+            maksumus on 40 €. ARK eksamile lubatakse vaid neid õpilasi, kes on
+            läbinud esmaabikoolitust. Need, kes ei saa mingil põhjusel läbida
+            koolitust tähtaegselt, võivad kirjutada avaldust ja lõpetada kursust
+            siis, kui selleks on neil aega. Alates 01.01.2012.a. esmaõppe
+            algaste koolituse käigus tuleb läbida sõidu libeda tee tingimustes -
+            harjutused libedasõidu õppeväljakul. Hind - 100 €.
+          </li>
+        </ol>
+        <p>
+          Alates 01.01.2012.a. Kui esmaõppe algaste koolitus läbiti ajal, millal
+          ei olnud pimeda aega, siis tuleb pimeda aja koolitus läbida esimesel
+          võimalusel pärast esmaõppe algaste läbimist enne lõppastme koolituse
+          algust. Hind - 100 €.
+        </p>
+      </section>
+      <section>
+        <p>
+          Lisatunnid – 1 lisaõppesõidukord (45 minutit) - 30 €. (FIE
+          instruktorile)
+        </p>
+        <p>Korduv sõidueksam - 30 €.(FIE instruktorile )</p>
+        <p>Korduv teooriaeksam - 30 €. (FIE õpetajale)</p>
+        <p>Õppesõiduplatsi kasutamine – 0.10 € / min.</p>
+      </section>
+      <section>
+        <p>Teoreetilised tunnid toimuvad aadressil Narva mnt 7B-317.</p>
+        <p>
+          Teoreetilised tunnid toimuvad ka elektroonilises keskkonnas Teooria.ee
+          või Liikluslab.ee.
+        </p>
+        <p>
+          Õpilane saab valida endale sobivaima süsteemi ja lahendada
+          kontrolltöid elektroonilises süsteemis.
+        </p>
+        <p>
+          Teenuste maksumus on märgitud Teooria.ee ja Liikluslab.ee
+          kodulehtedel.
+        </p>
+        <p>
+          Teooriaeksamile lubatakse kõik teemad läbinud ja vahekatsed sooritanud
+          õpilased.
+        </p>
+      </section>
+      <section>
+        <p>Õppemaksu tasumise kord</p>
+        <p>
+          Õpilane saab osaleda koolituskursustel juhul, kui ta on Autokooli
+          vastu võetud ning õppemaks on tasutud vastavalt kehtivale
+          hinnakirjale. Hinnakiri on Lepingu osa, kus on ära märgitud Autokooli
+          poolt Õpilasele osutatavate Teenuste hinnad.
+        </p>
+        <p>Lisateenuste hinnad on kehtestatud Autokooli hinnakirjas.</p>
+        <p>
+          Autokoolil on õigus kehtivat hinnakirja ühepoolselt muuta, teatades
+          hinnakirja muudatustest
+        </p>
+      </section>
+      <section>
+        <p>
+          Õpilane on kohustatud tasuma õppemaksu teooriakursuse eest vastavalt
+          kehtestatud Autokooli hinnakirja. Õppemaks tuleb tasuda seitsme (7)
+          kalendripäeva jooksul pärast Lepingu sõlmimist Autokooli
+          arvelduskontole või sularahas. Teooriakursuse eest on võimalik samuti
+          tasuda kahes osas Autokooli arvelduskontole või sularahas. Kahes osas
+          tasumise puhul on Õpilane kohustatud esimese sissemakse tegema seitsme
+          (7) kalendripäeva jooksul ning teise sissemakse 30 kalendripäeva
+          jooksul pärast Lepingu sõlmimist. Arvelduskontole makse tegemisel
+          tuleb märkida järgmised andmed:
+        </p>
+        <p>Saaja andmed: Alfa Pro OÜ</p>
+        <p>IBAN: EE362200221045532367 Swedbank</p>
+        <p>Selgitus: Õpilase ees- ja perekonnanimi, kelle eest tasutakse.</p>
+        <p>
+          Õppemaks loetakse tasutuks hetkest, mil Õpilase nimel tasutud summa
+          laekub Autokooli arvelduskontole või makstakse sularahas kohapeal
+          Autokoolis.
+        </p>
+      </section>
+      <section>
+        <p>
+          Võlgnevuse olemasolul, sh Õpilase poolse õppemaksu maksmise kohustuse
+          täitmisega viivitami sekorral, on Autokoolil õigus Õpilaselt nõuda
+          viivist 0,15% tasumisele kuuluvast summast, alates õppemaksu tasumise
+          kohustuse sissenõutavaks muutumisest (ehk alates seitsme (7)
+          kalendripäevale järgnevast kalendripäevast tasumata summast iga
+          tasumisega viivitatud kalendripäeva eest) kuni tasumisele kuuluva
+          summa täieliku tasumiseni. Õppemaks ei kuulu tagastamisele.
+        </p>
+      </section>
+    </>
+  );
+};
+
+export default Hinnad;
