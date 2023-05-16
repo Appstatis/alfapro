7e6f4823a633a88268f1c7905d062e93e038431d
Gibsol <gibsol75@gmail.com>
2023-05-16 17:13:25 +0300

    New LinkButton component implementation
    This component simply returns a daisyui card wrapped with Next Link to a
    specific url. The url (and also a name) are defined using props.

---
+++ src/components/pages/lingid/LinkButton.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export const LinkButton = ({ name, url }: { name: string; url: string }) => {
+  return (
+    <Link className="duration-200 hover:opacity-70" href={url}>
+      <div className="shadow-xl card bg-base-100">
+        <div className="card-body">
+          <h2 className="card-title">{name}</h2>
+          <p className="italic link">{url}</p>
+        </div>
+      </div>
+    </Link>
+  );
+};
