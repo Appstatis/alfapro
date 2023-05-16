fb0909ac25f566da1fbe558110940b155288b38c
Gibsol <gibsol75@gmail.com>
2023-05-16 17:17:00 +0300

    Min-h-screen in Layout
    Wrap all the Layout content except Footer in a section and add the
    `min-h-screen` class to it in order to make the Footer always at the
    very bottom and having its own space (User wont see it before scrolling
    down).

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -8,17 +8,19 @@ export const Layout = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
 
   return (
-    <div className="flex flex-col min-h-screen">
-      {router.pathname !== "/" && router.pathname !== "/404" && (
-        <Navbar title="AlfaPro" />
-      )}
-      <div
-        className={`${router.pathname !== "/404" &&
-          router.pathname !== "/" &&
-          "flex-1 mt-3 mb-2 md:mb-4 grid gap-5 px-4 py-1 sm:py-2 md:py-4 lg:py-6 xl:py-8 sm:px-16 md:px-16 lg:px-48 xl:px-64"}`}
-      >
-        <div className="flex flex-col gap-3">{children}</div>
-      </div>
+    <div className="flex flex-col">
+      <section className="min-h-screen">
+        {router.pathname !== "/" && router.pathname !== "/404" && (
+          <Navbar title="AlfaPro" />
+        )}
+        <div
+          className={`${router.pathname !== "/404" &&
+            router.pathname !== "/" &&
+            "flex-1 mt-3 mb-2 md:mb-4 grid gap-5 px-4 py-1 sm:py-2 md:py-4 lg:py-6 xl:py-8 sm:px-16 md:px-16 lg:px-48 xl:px-64"}`}
+        >
+          <div className="flex flex-col gap-3">{children}</div>
+        </div>
+      </section>
       <Footer title="Alfa Pro Autokool" />
     </div>
   );
