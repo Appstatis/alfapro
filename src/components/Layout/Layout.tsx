c4281b405b0e7c1f69f99fbf320515d04ea9ac14
Gibsol <gibsol@tutanota.com>
2023-05-19 08:52:53 +0300

    Layout: children wrap to main

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -14,11 +14,13 @@ export const Layout = ({ children }: { children: React.ReactNode }) => {
           <Navbar title="AlfaPro" />
         )}
         <div
-          className={`${router.pathname !== "/404" &&
+          className={`${
+            router.pathname !== "/404" &&
             router.pathname !== "/" &&
-            "flex-1 mt-3 mb-2 md:mb-4 grid gap-5 px-4 py-1 sm:py-2 md:py-4 lg:py-6 xl:py-8 sm:px-16 md:px-16 lg:px-48 xl:px-64"}`}
+            "flex-1 mt-3 mb-2 md:mb-4 grid gap-5 px-4 py-1 sm:py-2 md:py-4 lg:py-6 xl:py-8 sm:px-16 md:px-16 lg:px-48 xl:px-64"
+          }`}
         >
-          <div className="flex flex-col gap-3">{children}</div>
+          <main className="flex flex-col gap-3">{children}</main>
         </div>
       </section>
       <Footer title="Alfa Pro Autokool" />
